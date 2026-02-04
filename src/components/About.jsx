a9c27d4c9aceeaefa2a3848d28d43585aefb12b6
shreya-shambhavi01 <shreyashambhavimapa11@gmail.com>
2026-02-04 18:04:45 +0000

    removed config files and fixed tailwind v4

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
-import "./About.css";
+
 import "./Olympus.css"; // path apne project ke hisaab se
 
 export default function About() {
